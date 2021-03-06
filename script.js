b1ac2c02fee6bf247e417f698155b3958e47a8aa
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-07 02:05:47 +0500

    Created Donation Section

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 console.log("script.js");
 
 document.getElementById("signatureBtn").addEventListener("click", function(){
-    document.getElementById("signatureBtn").style.border = "1px solid black";
-    document.getElementById("signatureBtn").style.color = "#f77355";
+    document.getElementById("signatureBtn").style.border = "1px solid #2B416E";
+    document.getElementById("signatureBtn").style.color = "#2B416E";
     // 
     document.getElementById("buildYourOwnBagBtn").style.border = "1px solid #aaaaaa";
-    document.getElementById("buildYourOwnBagBtn").style.color = "black";
+    document.getElementById("buildYourOwnBagBtn").style.color = "#aaaaaa";
 })
 document.getElementById("buildYourOwnBagBtn").addEventListener("click", function(){
-    document.getElementById("buildYourOwnBagBtn").style.border = "1px solid black";
-    document.getElementById("buildYourOwnBagBtn").style.color = "#f77355";
+    document.getElementById("buildYourOwnBagBtn").style.border = "1px solid #2B416E";
+    document.getElementById("buildYourOwnBagBtn").style.color = "#2B416E";
 
     document.getElementById("signatureBtn").style.border = "1px solid #aaaaaa";
-    document.getElementById("signatureBtn").style.color = "black";
+    document.getElementById("signatureBtn").style.color = "#aaaaaa";
 })
 
 
